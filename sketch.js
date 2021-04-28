2a3c056798debca8d9819cc28322c86e1d0086f6
Aariz62 <78742034+Aariz62@users.noreply.github.com>
2021-04-28 13:28:51 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,12 +4,13 @@ var speed
 var weight
 
 function setup() {
-  createCanvas(800,400);
-  bullet=createSprite(400, 200, 50, 50);
-  bullet.shapecolour="green"
-   bullet.velocityX=speed
+  createCanvas(1600,800);
 
- wall=createSprite(700,200,60,height/2)
+  bullet=createSprite((400,400,50,50))
+  bullet.shapecolour="green"
+  bullet.velocityX=speed
+
+ wall=createSprite(1400,400,60,height/2)
  wall.shapecolour=250
 
  speed=random(55,90)
@@ -18,9 +19,9 @@ function setup() {
 }
 
 function draw() {
-  background(black); 
+  background("black"); 
   
-if(wall.x-car.x<car.width/2+wall.width/2){
+if(wall.x-bullet.x<bullet.width/2+wall.width/2){
   car.shapecolour="red"
 }
   drawSprites();
